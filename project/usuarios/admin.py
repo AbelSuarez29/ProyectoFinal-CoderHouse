914f8679a9d6ba1464bf1b23ceba6344a6b1f7ca
AbelSuarez29 <suarezabel139@gmail.com>
2023-12-24 03:42:01 -0300

    updated

--- project/usuarios/admin.py
+++ project/usuarios/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . import models
 
+admin.site.register(models.Usuarios)
 # Register your models here.
--- project/usuarios/models.py
+++ project/usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    nacimiento = models.DateField(null= True, blank= True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
