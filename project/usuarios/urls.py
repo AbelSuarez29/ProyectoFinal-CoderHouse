f7a7ffc578e79addc3f09875de54d315df67f8df
AbelSuarez29 <suarezabel139@gmail.com>
2023-12-25 22:48:08 -0300

    updated

--- project/usuarios/urls.py
+++ project/usuarios/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = "usuarios"
 
 urlpatterns = [
     path("", views.home, name = "index"),
     path("registro/", views.registro_view, name="registro"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", LogoutView.as_view(template_name="usuarios/logout.html"), name="logout"),
 ]
--- project/usuarios/views.py
+++ project/usuarios/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from .forms import UserCreationFormulario
 
+#------REGISTRAR USUARIOS--------#
+
 def home(request):
     return render(request, 'usuarios/index.html')
 
@@ -30,3 +32,46 @@ def registro_view(request):
                 "usuarios/index.html",
                 {"form": formulario}
             )
+        
+#---------LOGIN VIEW-----------#
+        
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return render(
+            request,
+            "core/index.html",
+            {"mensaje": f"Ya estás autenticado: {request.user.username}"}
+        )
+    
+    if request.method == "GET":
+        return render(
+            request, 
+            "usuarios/login.html",
+            {"form": AuthenticationForm()}
+        )
+    else:
+        formulario = AuthenticationForm(request, data = request.POST)
+
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            usuario = informacion["username"]
+            password = informacion["password"]
+            modelo = authenticate(username=usuario, password=password)
+            login(request, modelo)
+            return render(
+                request,
+                "core/index.html",
+            {"mensaje": f"bienvenido {modelo.username}"}
+            )
+        else:
+            return render(
+                request,
+                "usuario/login.html",
+                {"form": formulario}
+            )
+
+def logout_view(request):
+    pass
